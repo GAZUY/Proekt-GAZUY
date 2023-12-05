220595e1fb22e7abf20aa6447ffdf60223d50a52
GAZUY <gazuyd@gmail.com>
2023-12-05 20:35:02 +0300

    all

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
+import { resolve } from 'path'
+
 export default {
-  base: '/Proekt-GAZUY/'
+  base: '/Proekt-GAZUY/',
+  build: {
+    rollupOptions: {
+      input: {
+
+        // @ts-ignore
+        kino: resolve(__dirname, 'kino.html'),
+      }
+    }
+  }
 }
