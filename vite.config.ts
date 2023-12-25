6b78e57a9e07e7e8df6e9f6b0891c3a7d6676956
GAZUY <gazuyd@gmail.com>
2023-12-25 19:58:03 +0300

    Добавил папки ТИТУЛЬНИКА

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default {
       input: {
 
         // @ts-ignore
+        Titul: resolve(__dirname, 'Titul.html'),
+        // @ts-ignore
         kino: resolve(__dirname, 'kino.html'),
         // @ts-ignore
         yaPic: resolve(__dirname, 'yapic.html'),
